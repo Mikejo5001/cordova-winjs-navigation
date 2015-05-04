849b4c3c037782ff078168d1341c200e21871af3
Mikejo5001 <m.jones168@outlook.com>
2015-05-04 16:12:41 -0700

    initial commit

---
+++ WinJS_Navigation/platforms/android/assets/www/pages/home/home.js
@@ -0,0 +1,32 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+
+            var elem = document.querySelector(".navButton");
+            elem.addEventListener('click', this.btnHandler);
+
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        },
+
+        btnHandler: function (args) {
+            WinJS.Navigation.navigate('pages/page2/page2.html', args);
+        }
+
+    });
+})();
